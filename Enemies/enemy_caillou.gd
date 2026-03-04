a58dbd5ddb8eaf109f6fb60b07af4f9feb5a1bbd
Azhrilla <nico.claude@free.fr>
2026-03-04 10:05:10 +0100

    expose caillou vars

--- Enemies/enemy_caillou.gd
+++ Enemies/enemy_caillou.gd
@@ -1,17 +1,18 @@
 extends Enemy
-const MAX_HEALTH:int  = 40 
-var ATK_DMG:int = 10
+
 @export var m_baseDmg:int = 4
 @export var m_highDmg:int = 6
+@export var m_maxHealth:int = 40
+@export var m_armorGainedWhenHit:int = 5
 
 func _ready() -> void:
-	m_maximumHealth = MAX_HEALTH
+	m_maximumHealth = m_maxHealth
 	m_intentions=[Intention_SimpleDmg.new(m_baseDmg),Intention_SimpleDmg.new(m_highDmg)]
 	m_defaultWeightsIntentions = [3,2]
 	super._ready()
 
 func onDamageTaken(_effectiveDmg:int,_attacker:Character):
 	super.onDamageTaken(_effectiveDmg,_attacker)
-	addToStatusVariable(Globals.statusType.ARMOR,5)
+	addToStatusVariable(Globals.statusType.ARMOR,m_armorGainedWhenHit)
 	$AnimationPlayer.play("Parry")
 	$AnimationPlayer.queue("Idle")
